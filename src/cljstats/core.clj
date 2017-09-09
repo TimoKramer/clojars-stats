e3cfbfd3749f1e1f647cc5bb360e339eb364b036
Timo Kramer <ich@timokramer.de>
2017-09-09 20:19:28 +0200

    trying out some hash-map to merge-with the days

--- src/cljstats/core.clj
+++ src/cljstats/core.clj
@@ -6,7 +6,7 @@
 
 (defn yesterday [] (jtime/minus now (jtime/days 1)))
 
-(defn last-five-days [] (take 5 (jtime/iterate jtime/minus now (jtime/days 1))))
+(defn last-five-days [] (take 5 (jtime/iterate jtime/minus (yesterday) (jtime/days 1))))
 
 (defn format-date [local-date] (jtime/format "yyyyMMdd" local-date))
 
@@ -14,6 +14,12 @@
 
 (defn vectorize [stats] (map (fn [tuple] (vector (key tuple) (reduce + (vals (val tuple))))) stats))
 
+(defn hashmapize [stats] (map (fn [tuple] (hash-map (key tuple) (reduce + (vals (val tuple))))) stats))
+
 (defn sort-stats-descending [stats] (sort-by second > stats))
 
 (defn sort-stats-ascending [stats] (sort-by second < stats))
+
+(defn get-stats [date] (sort-stats-ascending (vectorize (read-stats (format-date date)))))
+
+(defn get-stats-l5d [days] (map get-stats (days)))
