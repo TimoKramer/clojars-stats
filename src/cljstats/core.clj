6237c22956024901ba8d3fda820fa1be9b2947d0
Timo Kramer <ich@timokramer.de>
2017-09-17 12:40:13 +0200

    working merge of different days

--- src/cljstats/core.clj
+++ src/cljstats/core.clj
@@ -6,6 +6,8 @@
 
 (defn yesterday [] (jtime/minus now (jtime/days 1)))
 
+(defn dbyesterday [] (jtime/minus now (jtime/days 2)))
+
 (defn last-five-days [] (take 5 (jtime/iterate jtime/minus (yesterday) (jtime/days 1))))
 
 (defn format-date [local-date] (jtime/format "yyyyMMdd" local-date))
@@ -14,12 +16,13 @@
 
 (defn vectorize [stats] (map (fn [tuple] (vector (key tuple) (reduce + (vals (val tuple))))) stats))
 
-(defn hashmapize [stats] (map (fn [tuple] (hash-map (key tuple) (reduce + (vals (val tuple))))) stats))
+(defn hashmapize [stats] (into (hash-map) (map (fn [tuple] {(key tuple) (reduce + (vals (val tuple)))}) stats)))
 
 (defn sort-stats-descending [stats] (sort-by second > stats))
 
 (defn sort-stats-ascending [stats] (sort-by second < stats))
 
-(defn get-stats [date] (sort-stats-ascending (vectorize (read-stats (format-date date)))))
+(defn get-stats [date] (hashmapize (read-stats (format-date date))))
 
-(defn get-stats-l5d [days] (map get-stats (days)))
+(defn get-stats-l5d [days] (let [stats (map get-stats days)]
+  (merge-with + (nth stats 0) (nth stats 1) (nth stats 2) (nth stats 3) (nth stats 4))))
